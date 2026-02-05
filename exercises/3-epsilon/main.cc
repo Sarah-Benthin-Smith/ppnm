6178c2a53eb8207c40bc02327ad7cad86f5899ec
Sarah Benthin Smith <au722512@uni.au.dk>
2026-02-05 11:47:00 +0100

    Jeg har lavet en opgave jubi

--- exercises/3-epsilon/main.cc
+++ exercises/3-epsilon/main.cc
@@ -1,5 +1,22 @@
-#include"hello.h"
-int main(){
-	hello();
-	return 0;
+#include <cstdio>
+#include<limits>
+#include <iostream>
+#include<cmath>
+
+
+int main() {
+float       f=1.0f; while((float)      (1.0f+f) != 1.0f){f/=2.0f;} f*=2.0f;
+double      d=1.0d; while((double)     (1.0d+d) != 1.0d){d/=2.0d;} d*=2.0d;
+long double l=1.0L; while((long double)(1.0L+l) != 1.0L){l/=2.0L;} l*=2.0L;
+std::printf("      float eps=%g\n",f);
+std::printf("     double eps=%g\n",d);
+std::printf("long double eps=%Lg\n",l);
+std::cout << std::numeric_limits<float>::epsilon() << "\n";
+std::cout << std::numeric_limits<double>::epsilon() << "\n";
+std::cout << std::numeric_limits<long double>::epsilon() << "\n";
+double power252=std::pow(2, -52);
+std::cout << "2^-52 = " << power252 << std::endl;
+double power223=std::pow(2, -23);
+std::cout << "2^-23 = " << power223 << std::endl;
+// return 0;
 }
