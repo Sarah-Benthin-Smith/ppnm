213fcf97a103122f8454299d8c15a0eb631ac92e
Sarah Benthin Smith <au722512@uni.au.dk>
2026-02-05 14:03:53 +0100

    Nye opgaver blev tilføjet, så skal der pushes igen

--- exercises/2-math/main.cc
+++ exercises/2-math/main.cc
@@ -1,22 +1,28 @@
 #include<iostream>
 #include<cmath>
+#include<complex>
 #include<cstdio>
 #include<numbers>
 #include"hello.h"
 #include"sfuns.h"
+using complex=std::complex<double>;
+
+constexpr double PI = std::numbers::pi;
+constexpr double EXP = std::numbers::e;
+constexpr complex I = complex(0,1);
 
 int main(){
 	hello();
-	constexpr double PI = std::numbers::pi;
-	constexpr double EXP = std::numbers::e;
-	double sqrt2=std::sqrt(2.0);
-	std::cout << "sqrt(2) = " << sqrt2 << std::endl;
-	double power2=std::pow(2.0, 0.2);
-	std::cout << "2^1/5 = " << power2 << std::endl;
-	double powerepi=std::pow(EXP, PI);
-	std::cout << "e^pi = " << powerepi << std::endl;
-	double powerpie=std::pow(PI, EXP);
-	std::cout << "pi^e = " << powerpie << std::endl;
+	std::cout << "sqrt(2) = " << std::sqrt(2.0) << std::endl;
+	std::cout << "2^1/5 = " << std::pow(2.0, 0.2) << std::endl;
+	std::cout << "e^pi = " << std::pow(EXP, PI) << std::endl;
+	std::cout << "pi^i = " << std::pow(PI, I) << std::endl;
+	std::cout << "pi^e = " << std::pow(PI, EXP) << std::endl;
+	std::cout << "pi^i = " << std::pow(PI, I) << std::endl;
+	std::cout << "e^i = " << std::pow(EXP, I) << std::endl;
+	std::cout << "i^i = " << std::pow(I, I) << std::endl;
+	std::cout << "log(i) = " << std::log(I) << std::endl;
+	
 	// double x=1;
 	// double y = sfuns::fgamma(x);
 	// std::cout << "fgamma(1)=" << y << "\n";
