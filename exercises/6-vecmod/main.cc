3e6309a26353557a06fdb7fe7658665c7cfcf47e
Sarah Benthin Smith <au722512@uni.au.dk>
2026-02-16 16:12:54 +0100

    vectors er nu mere fancy

--- exercises/6-vecmod/main.cc
+++ exercises/6-vecmod/main.cc
@@ -1,48 +1,36 @@
 #include "vec.h"
 #include <iostream>
 #include <iomanip>
+#include <complex>
 
 int main(){
 	using namespace vec_funs;
-    vec a(1,2,3);
-    vec b(4,5,6);
+    vec<double> a(1.123456789, 2.123456789, 3.123456789);
+    vec<float>  b(1.123456789f, 2.123456789f, 3.123456789f);
 
+    std::cout << "1. double and float vecs" << std::endl;
+    std::cout << "double vec = " << a << std::endl;
+    std::cout << "float vec = " << b << std::endl;
+
+    std::cout << "2. Mixing scalar types" << std::endl;
+	std::cout << "2*a = " << 2*a << "(integer*double)" << std::endl;
+
+    std::cout << "3. Adding complex numbers" << std::endl;	
+    using cd = std::complex<double>;
+    vec<cd> c(cd(1,1), cd(2,0), cd(0,3));
+    std::cout << "complex vec: " << c << std::endl;
+
+    std::cout << "4. Any type" << std::endl;	
+    vec<double> d(1.123456788, 2.123456788, 3.123456788);
     std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
+    std::cout << "d = " << d << std::endl;
 
-	// Testing addition
-	vec c = a + b;
-	std::cout << "a+b = " << c << std::endl;
-
-	// Testing subtraction
-	vec d = b - a;
-	std::cout << "b-a = " << d << std::endl;
-
-	// Testing multiplication
-	std::cout << "2*a = " << 2*a << std::endl;
-	std::cout << "a*3 = " << a*3 << std::endl;
-
-	// Testing division
-	std::cout << "a/2 = " << a/2 << std::endl;
-
-	// Testing negative vector
-	std:: cout << "-a = " << -a << std::endl;
-
-	// += operator
-    vec e = a;
-    e += b;
-    std::cout << "e += b -> " << e << std::endl;	
-
-	// approximation
-    vec f(1.0000001,2.0000001,3.0000001);
-    std::cout << std::setprecision(10);
-    std::cout << "f = " << f << std::endl;
-    if(approx(a,f)){
-        std::cout << "a approx f" << std::endl;
+    if(approx(a,d)){
+        std::cout << "approx works for vec<double>'s" << std::endl;
     }
 
     // print method
-    a.print("Vector a: ");
+    // a.print("Vector a: ");
 
     return 0;
 }
--- exercises/6-vecmod/vec.h
+++ exercises/6-vecmod/vec.h
@@ -1,46 +1,100 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <cmath>
 
-namespace vec_funs{
+namespace vec_funs
+{
+template<typename T>
+struct vec {
+    T x, y, z;
 
-    struct vec {
-        double x, y, z;
+    // constructors
+    vec(T x, T y, T z) : x(x), y(y), z(z) {}
+    vec() : vec(T{}, T{}, T{}) {}
 
-        // constructors
-        vec(double x, double y, double z) : x(x), y(y), z(z) {}  // parameterized
-        vec() : vec(0, 0, 0) {}                                    // default
-        vec(const vec&) = default;                                 // copy
-        vec(vec&&) = default;                                      // move
-        ~vec() = default;                                          // destructor
+    // arithmetic
+    vec& operator+=(const vec& other){
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+    vec& operator-=(const vec& other){
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
+    vec& operator*=(T n){
+        x *= n;
+        y *= n;
+        z *= n;
+        return *this;
+    }
+    vec& operator/=(T n){
+        x /= n;
+        y /= n;
+        z /= n;
+        return *this;
+    }
+    // utility
+    void set(T a, T b, T c){
+        x = a; y = b; z = c;
+    }
+    void print(const std::string& s = "") const {
+        std::cout << s << x << " " << y << " " << z << std::endl;
+    }
+    // stream output
+    friend std::ostream& operator<<(std::ostream& os, const vec& v){
+        return os << "{ " << v.x << ", " << v.y << ", " << v.z << " }";
+    }
+};
 
-        // assignment
-        vec& operator=(const vec&) = default;                     // copy assignment
-        vec& operator=(vec&&) = default;                          // move assignment
-
-        // arithmetic
-        vec& operator+=(const vec&);
-        vec& operator-=(const vec&);
-        vec& operator*=(double);
-        vec& operator/=(double);
-
-        // utility
-        void set(double a, double b, double c) { x = a; y = b; z = c; }
-        void print(const std::string& s = "") const;              // for debugging
-
-        // stream output
-        friend std::ostream& operator<<(std::ostream&, const vec&);
-    };
-
-    // non-member operators
-    vec operator-(const vec&);
-    vec operator-(const vec&, const vec&);
-    vec operator+(const vec&, const vec&);
-    vec operator*(const vec&, double);
-    vec operator*(double, const vec&);
-    vec operator/(const vec&, double);
-
-    // approximate equality
-    bool approx(const vec&, const vec&, double acc = 1e-6, double eps = 1e-6);
-
+// NON-MEMBER OPERATORS
+template<typename T>
+vec<T> operator+(vec<T> a, const vec<T>& b){
+    a += b;
+    return a;
 }
+template<typename T>
+vec<T> operator-(vec<T> a, const vec<T>& b){
+    a -= b;
+    return a;
+}
+template<typename T>
+vec<T> operator-(const vec<T>& a){
+    vec<T> r = a;
+    r *= static_cast<T>(-1);
+    return r;
+}
+template<typename T, typename S>
+vec<T> operator*(vec<T> a, S n){
+    a *= static_cast<T>(n);
+    return a;
+}
+template<typename T, typename S>
+vec<T> operator*(S n, vec<T> a){
+    a *= static_cast<T>(n);
+    return a;
+}
+template<typename T, typename S>
+vec<T> operator/(vec<T> a, S n){
+    a /= static_cast<T>(n);
+    return a;
+}
+// APPROX
+template<typename T>
+bool approx(T a, T b, double acc, double eps){
+    using std::abs;
+    if(abs(a-b) < acc) return true;
+    if(abs(a-b) < eps*(abs(a)+abs(b))) return true;
+    return false;
+}
+template<typename T>
+bool approx(const vec<T>& u, const vec<T>& v, double acc=1e-6, double eps=1e-6){
+    return approx(u.x,v.x,acc,eps)
+        && approx(u.y,v.y,acc,eps)
+        && approx(u.z,v.z,acc,eps);
+}
+} // namespace vec_funs
