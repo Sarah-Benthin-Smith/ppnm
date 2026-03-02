eee41a6e49e08bf0ab42c1944b3bdd5d52a0e3fd
Sarah Benthin Smith <au722512@uni.au.dk>
2026-03-02 08:57:34 +0100

    errorfunc is created ya!

---
+++ exercises/7-plots/funcs.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cmath>
+
+namespace funcs{
+
+double erf(double x);
+double sgamma(double x);
+double lngamma(double x);
+
+}
--- exercises/7-plots/main.cc
+++ exercises/7-plots/main.cc
@@ -1,36 +1,27 @@
-#include "vec.h"
+#include "funcs.h"
 #include <iostream>
 #include <iomanip>
 #include <complex>
+#include <cmath>
 
-int main(){
-	using namespace vec_funs;
-    vec<double> a(1.123456789, 2.123456789, 3.123456789);
-    vec<float>  b(1.123456789f, 2.123456789f, 3.123456789f);
-
-    std::cout << "1. double and float vecs" << std::endl;
-    std::cout << "double vec = " << a << std::endl;
-    std::cout << "float vec = " << b << std::endl;
-
-    std::cout << "2. Mixing scalar types" << std::endl;
-	std::cout << "2*a = " << 2*a << "(integer*double)" << std::endl;
-
-    std::cout << "3. Adding complex numbers" << std::endl;	
-    using cd = std::complex<double>;
-    vec<cd> c(cd(1,1), cd(2,0), cd(0,3));
-    std::cout << "complex vec: " << c << std::endl;
-
-    std::cout << "4. Any type" << std::endl;	
-    vec<double> d(1.123456788, 2.123456788, 3.123456788);
-    std::cout << "a = " << a << std::endl;
-    std::cout << "d = " << d << std::endl;
-
-    if(approx(a,d)){
-        std::cout << "approx works for vec<double>'s" << std::endl;
-    }
-
-    // print method
-    // a.print("Vector a: ");
-
-    return 0;
+int main(int argc,char** argv){
+	double xmin=0,xmax=10,dx=0.125;
+	for(int i=0;i<argc;i++){
+		std::string arg=argv[i];
+		std::cerr<<"i= "<<i<<" arg="<<arg<<"\n";
+		if(arg=="-xmin" && i+1<argc)xmin=std::stod(argv[i+1]);
+		if(arg=="-xmax" && i+1<argc)xmax=std::stod(argv[i+1]);
+		if(arg=="-dx" && i+1<argc)dx=std::stod(argv[i+1]);
+		}
+	std::cerr<<"xmin= "<<xmin<<"\n";
+	std::cerr<<"xmax= "<<xmax<<"\n";
+	std::cerr<<"dx= "<<dx<<"\n";
+	std::cout<<std::scientific;
+	for(double x=xmin;x<=xmax;x+=dx){
+		std::cout<<x<<" "<<funcs::erf(x)<<" "<<std::sin(x)<<"\n";
+	}
+    // std::cout << x << " "
+    //       << funcs::erf(x) << " "
+    //       << std::sin(x) << "\n";
+	return 0;
 }
