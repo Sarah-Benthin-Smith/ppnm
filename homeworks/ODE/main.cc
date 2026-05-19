4ff4302bfb225e69c3b5b18986d7c0530f9aa004
Sarah Benthin Smith <au722512@uni.au.dk>
2026-05-19 10:32:25 +0200

    nu er der 6 point

--- homeworks/ODE/main.cc
+++ homeworks/ODE/main.cc
@@ -20,6 +20,21 @@ vector f(double x, vector y){
     return dydx;
 }
 
+vector pend(double t, vector y1){
+    (void)t;
+    double b = 0.25;
+    double c = 5.0;
+    vector dydt(2);
+
+    double theta = y1[0];
+    double omega = y1[1];
+
+    dydt[0] = omega;
+    dydt[1] = -b*omega - c*std::sin(theta);
+
+    return dydt;
+}
+
 int main(){
 
     vector y0(2);
@@ -43,6 +58,29 @@ int main(){
             << ys[i][0] << " "
             << cos(xs[i]) << "\n";
     }
+
+    vector y10(2);
+    y10[0] = M_PI - 0.1;
+    y10[1] = 0.0;
+
+    auto [ts, ys1] = driver(
+        pend,
+        0.0,
+        10.0,
+        y0,
+        0.1,
+        1e-4,
+        1e-4
+    );
+
+    std::ofstream pendout("pendulum.data");
+
+    for(size_t i=0;i<ts.size();i++){
+        pendout << ts[i] << " "
+                << ys1[i][0] << " "   // theta
+                << ys1[i][1] << "\n"; // omega
+    }
+
 
     return 0;
 }
